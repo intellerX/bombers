7527ae4767ccfa04d4bf03f8557a1acd92ff1b23
intellerX <anmevel10@gmail.com>
2020-10-26 00:06:46 -0500

    Pictures

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => (
 
 <Card>
   <Card.Body>
-    Benemérito Cuerpo de Bomberos Voluntarios de Anserma Nuevo -Valle del Cauca Dirección: Cl. 6 #25, Ansermanuevo, Puerto Rondón, Arauca. Horarios de atención: de Lunes a viernes de 8:00 a.m a 12:00 p.m y de 2:00 p.m a 6:00 p.m / Sábados de 8:00 a.m. a 12 p.m Teléfono de Almacén de Prevención: Celular 311121311311  Teléfono de Emergencias: 44444444 - 2758032  Email: Bomberos@hotmail.com 
+    Benemérito Cuerpo de Bomberos Voluntarios de Anserma Nuevo -Valle del Cauca Dirección: Cra 3 # 7-40, Ansermanuevo, centro. Horarios de atención: de Lunes a viernes de 8:00 a.m a 12:00 p.m y de 2:00 p.m a 6:00 p.m / Sábados de 8:00 a.m. a 12 p.m Teléfono de Almacén de Prevención: Celular 311121311311  Teléfono de Emergencias: 44444444 - 2758032  Email: Bomberos@hotmail.com 
 
   </Card.Body>
 </Card>
